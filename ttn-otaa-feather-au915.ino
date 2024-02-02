696203b67db1a9e3bcb802f95f8eab611ee8eff2
Levi <fulcan19@hotmail.com>
2024-02-02 12:01:05 +1000

    Set Test-Device: APPEUI, DEVEUI and APPKEY for OTAA

--- ttn-otaa-feather-au915.ino
+++ ttn-otaa-feather-au915.ino
@@ -77,19 +77,19 @@ const unsigned TX_INTERVAL = 60;
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
 // 0x70.
-static const u1_t PROGMEM APPEUI[8]= { FILLMEIN };
+static const u1_t PROGMEM APPEUI[8]= { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 
 // End-device Identifier (u1_t[8]) in lsb format
 // This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]= { FILLMEIN };
+static const u1_t PROGMEM DEVEUI[8]= { 0xF3, 0xC2, 0xE8, 0x23, 0x2C, 0x3A, 0x23, 0x74 };
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 
 // Application Key (u1_t[16]) in msb format
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from the TTN console can be copied as-is.
-static const u1_t PROGMEM APPKEY[16] = { FILLMEIN };
+static const u1_t PROGMEM APPKEY[16] = { 0x75, 0x91, 0x12, 0xF7, 0xCE, 0xD1, 0x23, 0x82, 0x1A, 0x06, 0xDB, 0x51, 0x2A, 0x90, 0x92, 0xD2 };
 void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
 
 // EPOCH Time Defines
